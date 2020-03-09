0368c4054b1c8f248e7d4647d47a22e6079f0bb8
Julien Genestoux <julien.genestoux@gmail.com>
2020-03-08 20:35:01 -0400

    adding unlock

---
+++ src/hooks/useLocks.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+
+export const useLocks = (locks = []) => {
+  const [loading, setLoading] = useState(true)
+  const [locked, setLocked] = useState(true)
+
+  const unlock = () => {
+    window.unlockProtocol.loadCheckoutModal()
+  }
+
+  useEffect(() => {
+    const eventHandler = (event) => {
+      setLocked(event.detail === 'locked')
+      setLoading(false)
+    }
+
+    // Sets listener
+    window.addEventListener('unlockProtocol', eventHandler)
+
+    // resets the config
+    window.unlockProtocol.resetConfig({
+      locks: locks.reduce((acc, x) => {
+        return {
+          ...acc,
+          [x]: {}
+        }
+      }, {}),
+      callToAction: {
+      }
+    });
+
+    return () => {
+      // Cleanup
+      window.removeEventListener('unlockProtocol', eventHandler)
+    }
+  }, [locks])
+  return {loading, locked, unlock}
+}
+
