bc6370b79b7280c2a3ef491e87fb95f1905f9e22
Julien Genestoux <julien.genestoux@gmail.com>
2020-03-08 21:51:58 -0400

    udpated home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export const Home = () => {
-  return <p>Hello World</p>
+  return <p><a href="https://locked.fyi/">Locked.fyi</a> is a <em>decentralized blogging platform</em>! The content is stored on <a href="https://ipfs.io/">IPFS</a>, through the use of <a href="https://3box.io/">3Box</a>. Most importantly, it lets authors lock  their notes if they want to monetize them, using the <a href="https://unlock-protocol.com/">Unlock Protocol</a>!</p>
 }
 export default Home
