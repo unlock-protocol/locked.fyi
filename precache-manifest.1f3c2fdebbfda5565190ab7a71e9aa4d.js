1441fe574fd7b1f5b4adb5d468be9e98f7f8ad32
Julien Genestoux <julien.genestoux@gmail.com>
2020-04-15 21:56:33 -0400

    Updates

---
+++ precache-manifest.1f3c2fdebbfda5565190ab7a71e9aa4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd3b32896ac16c10b46a19eae9a67a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07ff6128d572a32e83c",
+    "url": "/static/css/2.04c88cd9.chunk.css"
+  },
+  {
+    "revision": "50b14be9d2518746c117",
+    "url": "/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "b07ff6128d572a32e83c",
+    "url": "/static/js/2.a6d5210f.chunk.js"
+  },
+  {
+    "revision": "8aeedadfcc63b8a6b227caa83ef8c044",
+    "url": "/static/js/2.a6d5210f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b14be9d2518746c117",
+    "url": "/static/js/main.58850f8f.chunk.js"
+  },
+  {
+    "revision": "1a1eb5432148990a6341",
+    "url": "/static/js/runtime-main.6cb33b11.js"
+  }
+]);
