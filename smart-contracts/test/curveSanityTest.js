3c6874813b06c79bf9d631b25d4ae309ba151876
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-17 16:27:18 -0600

    Add module lock js and work on contract

--- smart-contracts/test/curveSanityTest.js
+++ smart-contracts/test/curveSanityTest.js
@@ -1,5 +1,6 @@
 const { ethers } = require('@nomiclabs/buidler')
 const { assert } = require('chai')
+const { constants } = require('ethers')
 
 const DENOMINATOR = Math.pow(2, 64)
 const CURVE_MODIFIER = 3.321
@@ -16,16 +17,41 @@ function fixedPointToDecimal(int128Numerator) {
 
 describe('BondingCurveHook', () => {
   before(async () => {
+    const [address1, address2, address3] = await ethers.getSigners()
     const BondingCurveHook = await ethers.getContractFactory('BondingCurveHook')
+    const ImprovedBondingCurveHook = await ethers.getContractFactory(
+      'BondingCurveHook'
+    )
     hook = await BondingCurveHook.deploy()
+    hook = await improvedBondingCurveHook.deploy()
     await hook.deployed()
+    await improvedHook.deployed()
   })
 
-  it('Should return the correct price', async () => {
+  it('Should return the correct price from temp function', async () => {
     const supply = 10
     const tx = await hook.calculateKeyPrice(supply)
     const receipt = await tx.wait(1)
     console.log(`New Price: ${receipt.events[0].args._newPrice.toString()}`)
+    const numerator = await hook.tokenPrice.call()
+    const price = fixedPointToDecimal(numerator)
+    const calculatedPrice = jsPriceCalculator(supply)
+    console.log(`Num: ${numerator}`)
+    console.log(`Denom: ${DENOMINATOR}`)
+    console.log(`Price: ${price}`)
+    assert.equal(price, calculatedPrice)
+  })
+
+  it('Should return the correct price', async () => {
+    const tx = await improvedHook.keyPurchasePrice(
+      address1,
+      address1,
+      constants.ZeroAddress,
+      []
+    )
+    const receipt = await tx.wait(1)
+    console.log(`Supply: ${receipt.events[0].args._supply.toString()}`)
+    console.log(`Price: ${receipt.events[1].args._price.toString()}`)
     const numerator = await hook.tokenPrice.call()
     const price = fixedPointToDecimal(numerator)
     const calculatedPrice = jsPriceCalculator(supply)
