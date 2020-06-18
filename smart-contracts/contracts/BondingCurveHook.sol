f62c84859edf9e39dcec1e716b32ab46d51b21f3
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-18 10:27:01 -0600

    Work on hook contract

--- smart-contracts/contracts/BondingCurveHook.sol
+++ smart-contracts/contracts/BondingCurveHook.sol
@@ -6,8 +6,7 @@ pragma solidity ^0.5.17;
  * This contract is meant to be registered as a hook,
  * to be called on each key purchase from a Lock contract.
  * It tracks the token(key) supply for the lock, and adjusts
- * the price as the supply grows according to a binary log function,
- * P = log2(S) / 3.321, which aproximates a base-10 logarithm closely.
+ * the price as the supply grows according to a binary log function.
  */
 
 import '@unlock-protocol/unlock-abi-7/ILockKeyPurchaseHookV7.sol';
@@ -17,72 +16,65 @@ import 'abdk-libraries-solidity/ABDKMath64x64.sol';
 
 contract BondingCurveHook is ILockKeyPurchaseHookV7 {
 
+  // ////////////////////  Libs  ///////////////////////////
+
   using ABDKMath64x64 for int128;
   using ABDKMath64x64 for uint256;
+
+
+  // ////////////////////  Data  ///////////////////////////
 
   // The number of keys sold using this hook.
   int128 public tokenSupply;
 
-  // temporary. no need to store this, calculate as needed.
-  int128 public tokenPrice;
+  // mainnet address for deploy locked.fyi lock
+  address private constant LOCK_ADDRESS = 0xaad5Bff48e1534EF1f2f0A4184F5C2E61aC47EC3;
 
-  // what do I want to log here? price, supply or both? Price can always be calculated offchain... Supply can be looked up as needed onchain as well.
-  event PriceIncrease(int128 indexed _newPrice);
-
-/**
- * https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol#L366
- * Smart contract library of mathematical functions operating with signed
- * 64.64-bit fixed point numbers.  Signed 64.64-bit fixed point number is
- * basically a simple fraction whose numerator is signed 128-bit integer and
- * denominator is 2^64.  As long as denominator is always the same, there is no
- * need to store it, thus in Solidity signed 64.64-bit fixed point numbers are
- * represented by int128 type holding only the numerator.
- */
-
-  // The 64.64-bit representation of 3.321
-  // (61261637068789420000 / 2^64 = 3.321)
-  // The bonding curve we use here is P=log2(S)/3.321,
-  // which aproximates a base-10 logarithm.
+  /**
+  * 64.64-bit representation of 3.321:
+  * The curve function used is P=log2(S) / 3.321,
+  * which aproximates a base-10 logarithm.
+  * 3.321 * 2^64 = 61261637068789420000
+  * Details on how the fixed-point numbers work here:
+  * https://github.com/abdk-consulting/abdk-libraries-solidity/blob/939f0a264f2d07a9e2c7a3a020f0db2c0885dc01/ABDKMath64x64.sol#L8
+   */
   int128 private constant CURVE_MODIFER = 61261637068789420000;
 
+  //@audit Still needed ???
   // 2^64, as used by ABDKMath64x64.sol
   int128 private constant DENOMINATOR = 18446744073709552000;
 
+  // ////////////////////  Events  ///////////////////////////
 
+  // what do we want to log here?
+  event KeyPrice(int128 _supply, int128 _price);
 
-  function calculateKeyPrice(uint256 _supply)
-    public
-  {
-    int128 supplyAsInt;
-    supplyAsInt = _supply.fromUInt();
-    tokenPrice = supplyAsInt.log_2().div(CURVE_MODIFER);
-    emit PriceIncrease(tokenPrice);
-  }
-
+  // ////////////////////  Functions  ///////////////////////////
 
 /**
    * @notice Used to determine the purchase price before issueing a transaction.
    * This allows the hook to offer a discount on purchases.
    * This may revert to prevent a purchase.
-   * @param from the msg.sender making the purchase
-   * @param recipient the account which will be granted a key
-   * @param referrer the account which referred this key sale
-   * @param data arbitrary data populated by the front-end which initiated the sale
+   * param from the msg.sender making the purchase
+   * param recipient the account which will be granted a key
+   * param referrer the account which referred this key sale
+   * param data arbitrary data populated by the front-end which initiated the sale
    * @return minKeyPrice the minimum value/price required to purchase a key with these settings
    * @dev the lock's address is the `msg.sender` when this function is called via
    * the lock's `purchasePriceFor` function
    */
   function keyPurchasePrice(
-    address from,
-    address recipient,
-    address referrer,
-    bytes calldata data
+    address, //from,
+    address, //recipient,
+    address, //referrer,
+    bytes calldata //data
   ) external view
     returns (uint minKeyPrice)
   {
     // get the price for the lock before purchase ic completed.
-    // tokenPrice = tokenSupply.log_2().div(CURVE_MODIFER);
-    // return tokenPrice.toUInt();
+    int128 tokenPrice = tokenSupply.log_2().div(CURVE_MODIFER);
+    // emit KeyPrice(tokenPrice, tokenSupply);
+    return tokenPrice.toUInt();
   }
 
   /**
@@ -106,8 +98,14 @@ contract BondingCurveHook is ILockKeyPurchaseHookV7 {
     uint pricePaid
   ) external
   {
+    // Ensure caller is the locked.fyi lock !
+    require(msg.sender == LOCK_ADDRESS);
+
     // update current supply using internal counter
+    tokenSupply++;
     // pull author address from calldata
+    address author;
+    // author = data[];
     // inform DAO to mint new share for author
   }
 }
