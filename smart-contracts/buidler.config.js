81e1e4ed225cdfd9705225ae078cab3339f6159f
Nick Furfaro <nfurfaro33@gmail.com>
2020-06-25 16:11:24 -0600

    Remove old comments

--- smart-contracts/buidler.config.js
+++ smart-contracts/buidler.config.js
@@ -4,23 +4,6 @@ usePlugin('solidity-coverage')
 // usePlugin('buidler-gas-reporter')
 usePlugin('@nomiclabs/buidler-solhint')
 
-// const UnlockJSON = require('@unlock-protocol/unlock-abi-7/Unlock.json')
-// const LockJSON = require('@unlock-protocol/unlock-abi-7/PublicLock.json')
-// const HookJSON = require('../artifacts/BondingCurveHook.json')
-// const HookABI = HookJSON.abi
-// const UnlockABI = UnlockJSON.abi
-// const LockABI = LockJSON.abi
-// const UnlockBytecode = UnlockJSON.bytecode
-// const LockBytecode = LockJSON.bytecode
-// const HookBytecode = HookJSON.bytecode
-
-// async function artifactProcessor() {
-//   return {
-//     abi: [],
-//     bytecode: '0xabc..',
-//   }
-// }
-
 module.exports = {
   defaultNetwork: 'buidlerevm',
   networks: {
@@ -37,6 +20,5 @@ module.exports = {
     currency: 'USD',
     excludeContracts: [],
     gasPrice: 5,
-    // artifactType: artifactProcessor(),
   },
 }
