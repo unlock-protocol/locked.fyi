8018cf4955d368e9aeece117fda1eed1a52a8388
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-01 08:13:23 -0600

    Clean up Hook contract

--- smart-contracts/contracts/BondingCurveHook.sol
+++ smart-contracts/contracts/BondingCurveHook.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.5.17;
  * This contract is meant to be registered as a hook,
  * to be called on each key purchase from a Lock contract.
  * It tracks the token(key) supply for the lock, and adjusts
- * the price as the supply grows according to a binary log function.
+ * the price as the supply grows according to a modified binary log function.
  */
 
 import '@unlock-protocol/unlock-abi-7/ILockKeyPurchaseHookV7.sol';
@@ -16,7 +16,7 @@ import 'abdk-libraries-solidity/ABDKMath64x64.sol';
 import '@nomiclabs/buidler/console.sol';
 
 contract BondingCurveHook is ILockKeyPurchaseHookV7 {
-  // @audit add credit for ABDK LIB !!!
+
   // ////////////////////  Libs  ///////////////////////////
 
   using ABDKMath64x64 for int128;
@@ -25,55 +25,26 @@ contract BondingCurveHook is ILockKeyPurchaseHookV7 {
 
   // ////////////////////  Data  ///////////////////////////
 
-  // The number of keys sold using this hook.
+  // Number of keys sold using this hook.
   uint256 public tokenSupply;
 
-  // mainnet address for deploy locked.fyi lock
-  address private constant LOCK_ADDRESS = 0xaad5Bff48e1534EF1f2f0A4184F5C2E61aC47EC3;
+  // Address of paired lock
+  address private lockAddress;
 
-  //@audit for testing only. deploy a mock hook for this
-  address private _testLockAddress;
-
-  /**
-  * 64.64-bit representation of 3.321:
-  * The curve function used is P=log2(S) / 3.321,
-  * which aproximates a base-10 logarithm.
-  * 3.321 * 2^64 = 61261637068789420000
-  * Details on how the fixed-point numbers work here:
-  * https://github.com/abdk-consulting/abdk-libraries-solidity/blob/939f0a264f2d07a9e2c7a3a020f0db2c0885dc01/ABDKMath64x64.sol#L8
-   */
+  // For details: https://github.com/unlock-protocol/locked.fyi/blob/master/smart-contracts/Readme.md
   int128 private constant CURVE_MODIFER = 61278757397652720000;
-
-  //@audit Still needed ???
-  // 2^64, as used by ABDKMath64x64.sol
   int128 private constant DENOMINATOR = 18446744073709552000;
-
-  // ////////////////////  Events  ///////////////////////////
-
-  // what do we want to log here?
-  event KeyPrice(int128 _supply, int128 _price);
 
   // ////////////////////  Functions  ///////////////////////////
 
-  constructor(uint _initialSupply, address _testLock) public {
-    // needed to set supply > 0 (ABDK lib fails otherwise)
+  /// @param _initialSupply The initial value for tokenSupply
+  /// @param _lock The address of the lock configured to use this hook.
+  /// @dev _initialSupply must be > 0 to avoid a revert
+  constructor(uint _initialSupply, address _lock) public {
     tokenSupply = _initialSupply;
-    // @audit testing only, move to mock and remove arg from constructor
-    _testLockAddress = _testLock;
+    lockAddress = _lock;
   }
 
-/**
-   * @notice Used to determine the purchase price before issueing a transaction.
-   * This allows the hook to offer a discount on purchases.
-   * This may revert to prevent a purchase.
-   * param from the msg.sender making the purchase
-   * param recipient the account which will be granted a key
-   * param referrer the account which referred this key sale
-   * param data arbitrary data populated by the front-end which initiated the sale
-   * @return minKeyPrice the minimum value/price required to purchase a key with these settings
-   * @dev the lock's address is the `msg.sender` when this function is called via
-   * the lock's `purchasePriceFor` function
-   */
   function keyPurchasePrice(
     address from,
     address recipient,
@@ -81,12 +52,14 @@ contract BondingCurveHook is ILockKeyPurchaseHookV7 {
     bytes calldata data
   ) external view
     returns (uint minKeyPrice)
+  // solium-disable-next-line
   {
     // no-op.
   }
 
-  // https://ethereum.stackexchange.com/questions/15350/how-to-convert-an-bytes-to-address-in-solidity
+  // Source: https://ethereum.stackexchange.com/questions/15350/how-to-convert-an-bytes-to-address-in-solidity
   function bytesToAddress(bytes memory b) private pure returns (address addr) {
+  // solium-disable-next-line
     assembly {
       addr := mload(add(b,20))
     }
@@ -113,10 +86,7 @@ contract BondingCurveHook is ILockKeyPurchaseHookV7 {
     uint /*pricePaid**/
   ) external
   {
-    // Ensure caller is the locked.fyi lock:
-    // @audit re-enable check before deployment !!!
-    // require(msg.sender == LOCK_ADDRESS);
-    require(msg.sender == _testLockAddress);
+    require(msg.sender == lockAddress, 'UNAUTHORIZED_LOCK');
     IPublicLockV7 lock = IPublicLockV7(msg.sender);
     tokenSupply++;
 
@@ -127,8 +97,6 @@ contract BondingCurveHook is ILockKeyPurchaseHookV7 {
     // get current token address from lock:
     address tokenAddress = lock.tokenAddress();
     lock.updateKeyPricing(keyPrice, tokenAddress);
-    // @audit For testing only ! (adds to gas-usage)
-    // console.logBytes(data);
     address author = bytesToAddress(data);
     console.logAddress(author);
     // DAO.mint(data, 1);
