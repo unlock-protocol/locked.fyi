b0742b74040f200aff97b46c2b5ee95c0262c612
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-20 05:47:59 -0600

    Update buidler config for deployments

--- smart-contracts/buidler.config.js
+++ smart-contracts/buidler.config.js
@@ -3,12 +3,28 @@ usePlugin('buidler-ethers-v5')
 usePlugin('solidity-coverage')
 // usePlugin('buidler-gas-reporter')
 usePlugin('@nomiclabs/buidler-solhint')
+usePlugin('buidler-deploy')
+
+const mnemonic = process.env.MNEMONIC
+if (!mnemonic || mnemonic === '') {
+  throw new Error(`environment variable "MNEMONIC" not configured `)
+}
+
+const accounts = mnemonic
+  ? {
+      mnemonic,
+    }
+  : undefined
 
 module.exports = {
   defaultNetwork: 'buidlerevm',
   networks: {
     localhost: {
       url: 'http://localhost:8545',
+    },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/459caf4344dc4bd8a235559b3c4ee972',
+      accounts,
     },
   },
   solc: {
@@ -20,5 +36,22 @@ module.exports = {
     currency: 'USD',
     excludeContracts: [],
     gasPrice: 5,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 4,
+      1: '0x4011d09a86D0acA8377a4A8baD691F1ACeeCd672',
+      4: '0x4011d09a86D0acA8377a4A8baD691F1ACeeCd672',
+    },
+    lock: {
+      default: 4,
+      1: '',
+      4: '0x616a27F4447E563501630C728F36997A51D59b61',
+    },
+    tokenManager: {
+      default: 4,
+      1: '',
+      4: '0xd718388e922e5d23e3349dacb5d8a283f63f95e4',
+    },
   },
 }
