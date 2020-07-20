21a3949214d9c494f11f6fd4d8f4ba158e8cf27b
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-19 20:28:32 -0600

    Add minimal token manager interface

---
+++ smart-contracts/contracts/ITokenManager.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.17;
+
+interface ITokenManager {
+  /**
+    * @notice Mint `@tokenAmount(self.token(): address, _amount, false)` tokens for `_receiver`
+    * @param _receiver The address receiving the tokens, cannot be the Token Manager itself (use `issue()` instead)
+    * @param _amount Number of tokens minted
+    */
+    function mint(address _receiver, uint256 _amount) external;
+}
