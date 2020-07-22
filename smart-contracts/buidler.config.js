4b3cf2c58252ddde9955f23533caf5cc81eab9e1
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-21 23:26:42 -0600

    Add hook deployment

--- smart-contracts/buidler.config.js
+++ smart-contracts/buidler.config.js
@@ -4,12 +4,28 @@ usePlugin('solidity-coverage')
 // usePlugin('buidler-gas-reporter')
 usePlugin('@nomiclabs/buidler-solhint')
 usePlugin('@nomiclabs/buidler-etherscan')
+usePlugin('buidler-deploy')
+
+const mnemonic = process.env.MNEMONIC
+if (!mnemonic || mnemonic === '') {
+  throw new Error(`environment variable "MNEMONIC" not configured `)
+}
+
+const accounts = mnemonic
+  ? {
+      mnemonic,
+    }
+  : undefined
 
 module.exports = {
   defaultNetwork: 'buidlerevm',
   networks: {
     localhost: {
       url: 'http://localhost:8545',
+    },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/459caf4344dc4bd8a235559b3c4ee972',
+      accounts,
     },
   },
   solc: {
@@ -25,5 +41,27 @@ module.exports = {
   etherscan: {
     url: 'https://api-RINKEBY.etherscan.io/api',
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 4,
+      1: accounts[0],
+      4: accounts[0],
+    },
+    lock: {
+      default: 4,
+      1: '',
+      4: '0x616a27F4447E563501630C728F36997A51D59b61',
+    },
+    hook: {
+      default: 4,
+      1: '',
+      4: '0x17d3e2f064A09848b8C62FcDB6606E7330A8cdd2',
+    },
+    tokenManager: {
+      default: 4,
+      1: '',
+      4: '0xd718388e922e5d23e3349dacb5d8a283f63f95e4',
+    },
   },
 }
