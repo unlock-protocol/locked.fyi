e6decd8765898af62e36bc7878b677551a4c62e2
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-27 14:09:43 -0600

    Update deploy script

--- smart-contracts/deploy/01_deploy_tokenManager.js
+++ smart-contracts/deploy/01_deploy_tokenManager.js
@@ -2,13 +2,18 @@ module.exports = async ({getNamedAccounts, deployments}) => {
   const {deployIfDifferent, log} = deployments
 
   const {lock, deployer, tokenManager} = await getNamedAccounts()
+  console.log(`lock: ${lock}`)
+  console.log(`deployer: ${deployer}`)
+  console.log(`tokenManager: ${tokenManager}`)
+
+  const initialSupply = 42
 
   const deployResult = await deployIfDifferent(
     ['data'],
     'BondingCurveHook',
     {from: deployer, gas: 3000000},
     'BondingCurveHook',
-    42,
+    initialSupply,
     lock,
     tokenManager
   )
