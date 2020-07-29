0833cf77a68763bb0b20535a6e71e9ca51c23479
Nick Furfaro <nfurfaro33@gmail.com>
2020-07-28 21:01:51 -0600

    Add deployment script

---
+++ smart-contracts/deploy/01_deploy_tokenManager.js
@@ -0,0 +1,31 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deployIfDifferent, log } = deployments
+
+  const { lock, deployer, tokenManager } = await getNamedAccounts()
+  console.log(`lock: ${lock}`)
+  console.log(`deployer: ${deployer}`)
+  console.log(`tokenManager: ${tokenManager}`)
+
+  const initialSupply = 42
+
+  const deployResult = await deployIfDifferent(
+    ['data'],
+    'BondingCurveHook',
+    { from: deployer, gas: 3000000 },
+    'BondingCurveHook',
+    initialSupply,
+    lock,
+    tokenManager
+  )
+
+  if (deployResult.newlyDeployed) {
+    log(
+      ' - Hook deployed at : ' +
+        deployResult.address +
+        ' for gas : ' +
+        deployResult.receipt.gasUsed
+    )
+  } else {
+    log('reusing Hook at ' + deployResult.address)
+  }
+}
